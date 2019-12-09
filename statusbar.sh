051b394a2436244876f65a7bcff6749d200f8858
nyc4m <baptiste.prunot@utt.fr>
2019-12-09 07:54:30 +0100

    Replace emoji with font icon

--- statusbar.sh
+++ statusbar.sh
@@ -1,21 +1,25 @@
 #! /bin/bash
 
 function wifi() {
-    echo $(/usr/bin/nmcli dev wifi | grep -E '^[*]' | awk '/*/ {print $2}')
+    echo " $(/usr/bin/nmcli dev wifi | grep -E '^[*]' | awk '/*/ {print $2}')"
 }
 
 function customDate() {
-    echo $(date +"%A, %d %B %Y : %Hh%M")
+    echo " $(date +"%A, %d %B %Y : %Hh%M")"
 }
 
 function volume() {
-    echo $(amixer sget Master | grep -E -o "[0-9]{1,3}%")
+    echo " $(amixer sget Master | grep -E -o "[0-9]{1,3}%" -m 1)"
+}
+
+function battery() {
+    echo " $(</sys/class/power_supply/BAT$1/capacity)"
 }
 
 function customBar()
 {
 while true
 do
-    xsetroot -name "🔊$(volume) | $(wifi) | $(customDate)"
+    xsetroot -name "| $(battery 1) $(battery 0) | $(volume) | $(wifi) | $(customDate)"
 done
 }
